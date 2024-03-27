2044ed074bddaac25d5055bab546daac98e55fa2
Ronan <ronan.dupas@ynov.com>
2024-03-27 13:13:00 +0100

    change file Fight

--- Fight.ts
+++ Fight.ts
@@ -1,18 +1,27 @@
+import Character from "./Character.ts";
+
 class Fight{
 
-    adventurers : string[];
-    monsters : string[];
+    adventurers : Character[];
+    monsters : Character[];
 
-    constructor(adventurers : string[], monsters : string[]){
+    constructor(adventurers : Character[], monsters : Character[]){
         this.adventurers = adventurers;
         this.monsters = monsters;
     }
 
     orderOfTurns(){
+        const allCharacters = this.adventurers.concat(this.monsters);
+        allCharacters.sort((a, b) => b.speed - a.speed)
+    }
+
+    nextTurns(){
         
     }
 
-    endOfTheFigth(){
-        
+    isFightOver(){
+        if(this.adventurers.every(Character => Character.currentPv <= 0) || this.monsters.every(Character => Character.currentPv <= 0)){
+            return "The fight is over"
+        }
     }
 }
