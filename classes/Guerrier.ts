73b1c5e07d4e0bd4615addd832d22378a7383b24
CODEBYGODWIN <oblasseg18@gmail.com>
2024-04-14 13:00:36 +0200

    clean main

--- classes/Guerrier.ts
+++
@@ -1,9 +0,0 @@
-import Character from "./Character.ts";
-
-class Guerrier extends Character {
-    constructor(nom: string) {
-        super(nom, 20, 15, 10, 100, 100);
-    }
-}
-
-export default Guerrier;
